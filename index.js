f9e2efb72c9943587b8835f3c70ca4920d31a46c
FelipeGuimaraes1 <fil-2014@hotmail.com>
2024-01-08 10:47:54 -0300

    Segundo commit

--- index.js
+++ index.js
@@ -1,103 +1,107 @@
-const express = require('express')
-const uuid = require('uuid')
-const port = 3000
-const app = express()
-app.use(express.json())
+const express = require("express");
+const uuid = require("uuid");
+const port = 3000;
+const app = express();
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
 
-const orderItems = []
+const orderItems = [];
 
 const checkOrderId = (request, response, next) => {
-    const { id } = request.params
+  const { id } = request.params;
 
-    const index = orderItems.findIndex((order) => order.id === id)
+  const index = orderItems.findIndex((order) => order.id === id);
 
-    if(index < 0) {
-        return response.status(404).json({error: 'Order not found.'})
-    }
+  if (index < 0) {
+    return response.status(404).json({ error: "Order not found." });
+  }
 
-    request.orderIndex = index
-    request.orderId = id
+  request.orderIndex = index;
+  request.orderId = id;
 
-    next()
-}
+  next();
+};
 
 const showDetails = (request, response, next) => {
-    const fullUrl = `${request.protocol}://${request.hostname}:${request.socket.localPort}${request.originalUrl}`
-    
-    console.log(
+  const fullUrl = `${request.protocol}://${request.hostname}:${request.socket.localPort}${request.originalUrl}`;
+
+  console.log(
     `Método da requisição: ${request.method},
-URL da reuisição: ${fullUrl}`)
+URL da requisição: ${fullUrl}`
+  );
 
-    next()
-}
+  next();
+};
 
-app.post('/order', showDetails, (request, response) => {
-    const { order, clientName, price } = request.body
-    const orders = { id: uuid.v4(), order, clientName, price, status: 'Em preparação' }
+app.post("/order", showDetails, (request, response) => {
+  const { order, clientName, price } = request.body;
+  const orders = {
+    id: uuid.v4(),
+    order,
+    clientName,
+    price,
+    status: "Em preparação",
+  };
 
-    orderItems.push(orders)
+  orderItems.push(orders);
 
-    return response.status(201).json(orderItems)
-})
+  return response.status(201).json(orderItems);
+});
 
-app.get('/order', showDetails,(request, response) => {
-    return response.json(orderItems)
-})
+app.get("/order", showDetails, (request, response) => {
+  return response.json(orderItems);
+});
 
-app.put('/order/:id', showDetails, checkOrderId, (request, response) => {
-    console.log(request)
-    const { order, clientName, price } = request.body
+app.put("/order/:id", showDetails, checkOrderId, (request, response) => {
+  console.log(request);
+  const { order, clientName, price } = request.body;
 
-    const id = request.orderId
+  const id = request.orderId;
 
-    const updatedOrder = { id, order, clientName, price, status: 'Em preparação'}
+  const updatedOrder = {
+    id,
+    order,
+    clientName,
+    price,
+    status: "Em preparação",
+  };
 
-    const index = request.orderIndex
+  const index = request.orderIndex;
 
-    orderItems[index] = updatedOrder
+  orderItems[index] = updatedOrder;
 
-    return response.json(updatedOrder)
-})
+  return response.json(updatedOrder);
+});
 
-app.delete('/order/:id', showDetails, checkOrderId, (request, response) => {
-    const index = request.orderIndex
+app.delete("/order/:id", showDetails, checkOrderId, (request, response) => {
+  const index = request.orderIndex;
 
-    orderItems.splice(index,1)
+  orderItems.splice(index, 1);
 
-    return response.status(204).json()
-})
+  return response.status(204).json();
+});
 
-app.get('/order/:id', showDetails, checkOrderId, (request, response) => {
-    const index = request.orderIndex
+app.get("/order/:id", showDetails, checkOrderId, (request, response) => {
+  const index = request.orderIndex;
 
-    const filteredOrder = orderItems.slice(index, index + 1) [0]
-    
-    return response.json(filteredOrder)
-})
+  const filteredOrder = orderItems[index];
 
-app.patch('/order/:id', showDetails, checkOrderId, (request, response) => {
-    const index = request.orderIndex
+  return response.json(filteredOrder);
+});
 
-    const changeStatus = orderItems[index]
+app.patch("/order/:id", showDetails, checkOrderId, (request, response) => {
+  const index = request.orderIndex;
 
-    changeStatus.status = 'Pronto'
+  const changeStatus = orderItems[index];
 
-    orderItems[index] = changeStatus
+  changeStatus.status = "Pronto";
 
-    return response.json(changeStatus)
-})
+  orderItems[index] = changeStatus;
 
-
-
-
-
-
-
-
-
-
-
+  return response.json(changeStatus);
+});
 
 app.listen(port, () => {
-    console.log(`💻 Server is running on port ${port} 💻`)
-})
+  console.log(`💻 Server is running on port ${port} 💻`);
+});
